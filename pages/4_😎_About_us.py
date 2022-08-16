9ea2641d311dd459600b1f462b7beb7dfae313c8
alodiayusuf <44140954+alodiayusuf@users.noreply.github.com>
2022-08-16 21:03:52 +0800

    Update 4_😎_About_us.py

--- pages/4_😎_About_us.py
+++ pages/4_😎_About_us.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st 
 from streamlit_lottie import st_lottie
 
-st.set_page_config(page_title="About Us", page_icon="🕵️‍♀️")
+st.set_page_config(page_title="About Us", page_icon="😎")
 st.header("Print(awesome)")
 def load_lottieurl(url):
     r = requests.get(url)
